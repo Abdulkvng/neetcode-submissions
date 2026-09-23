e740ff1798aee904eb529564c9bf592b483e1cb0
Abdulrahman Sadiq <156849899+Abdulkvng@users.noreply.github.com>
2026-09-23 15:26:54 -0700

    Add: is-anagram - submission-33

---
+++ Data Structures & Algorithms/is-anagram/submission-33.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        return Counter(s) == Counter(t)
+
+        # cacheS = {}
+        # cacheT = {}
+
+        # for char in s:
+        #     if char in cacheS:
+        #         cacheS[char] += 1
+        #     else:
+        #         cacheS[char] = 1
+
+        # for char in t:
+        #     if char in cacheT:
+        #         cacheT[char] += 1
+        #     else:
+        #         cacheT[char] = 1
+
+        # return cacheS == cacheT
+
+
+
+        
+        
